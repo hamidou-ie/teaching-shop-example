1ba63beb9f9cc1434688a4b6eb009f6c433df059
hamikode <thdiallo.ie@gmail.com>
2026-02-05 17:14:36 +0100

    update package-lock18

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,2 +1,2 @@
 export const ADMIN_BASE_URL = "http://ec2-35-180-255-192.eu-west-3.compute.amazonaws.com/admin/";
-export const API_BASE = 'http://ec2-35-180-255-192.eu-west-3.compute.amazonaws.com';
+export const API_BASE = 'http://ec2-35-180-255-192.eu-west-3.compute.amazonaws.com:5000/api';
